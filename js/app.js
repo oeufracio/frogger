c6a9cd5cb245d313a845d341bb6cc8ac129d5173
oeufracio <odin.eufracio@gmail.com>
2016-12-27 16:17:14 -0600

    Use uppercase for constants and meet requirements for project

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 /*************** Game Object ***************/
 var Game = function() {
     this.board = {
-        "width": 505,       // Width of Canvas
-        "height": 606,      // Height of Canvas
-        "rows": 6,          // Total number of rows in the game
-        "cols": 5,          // Total number of cols in the game
-        "box_width": 101,   // Width of images
-        "box_height": 83,   // Height of images
-        "blocks": 3,        // Number of available rows for bugs
-        "enemies": 3,       // Number of bugs
-        "min_speed": 200,   // Min speed for bugs
-        "max_speed": 500,   // Max speed for bugs
-        "points_win": 7     // Points needed to win the game
+        "WIDTH": 505,       // Width of Canvas
+        "HEIGHT": 606,      // Height of Canvas
+        "ROWS": 6,          // Total number of rows in the game
+        "COLS": 5,          // Total number of cols in the game
+        "BOX_WIDTH": 101,   // Width of images
+        "BOX_HEIGHT": 83,   // Height of images
+        "BLOCKS": 3,        // Number of available rows for bugs
+        "ENEMIES": 3,       // Number of bugs
+        "MIN_SPEED": 200,   // Min speed for bugs
+        "MAX_SPEED": 500,   // Max speed for bugs
+        "POINTS_WIN": 7     // Points needed to win the game
     };
 
     this.gem = {
@@ -57,22 +57,22 @@ Game.prototype.update = function() {
     //With a relatively small probability a gem appears on on_board
     if( !this.state.finished && !this.gem.on_board && Math.random() < 0.001  ) {
         this.gem.on_board = true;
-        this.gem.x = Math.floor( this.board.cols * Math.random() );
-        this.gem.y = 1 + Math.floor( this.board.blocks * Math.random() );
+        this.gem.x = Math.floor( this.board.COLS * Math.random() );
+        this.gem.y = 1 + Math.floor( this.board.BLOCKS * Math.random() );
     }
     //With a relatively small probability a heart appears on on_board
     if( !this.state.finished && !this.heart.on_board && Math.random() < 0.001  ) {
         this.heart.on_board = true;
-        this.heart.x = Math.floor( this.board.cols * Math.random() );
-        this.heart.y = 1 + Math.floor( this.board.blocks * Math.random() );
+        this.heart.x = Math.floor( this.board.COLS * Math.random() );
+        this.heart.y = 1 + Math.floor( this.board.BLOCKS * Math.random() );
     }
 
     //The game finishes ff there is no lives or the player achieves the score
-    if( ! this.statistics.lives || this.statistics.score >= this.board.points_win ) {
+    if( ! this.statistics.lives || this.statistics.score >= this.board.POINTS_WIN ) {
         this.state.finished = true;
 
         this.state.msg = "GAME OVER";
-        if(  this.statistics.score >= this.board.points_win )
+        if(  this.statistics.score >= this.board.POINTS_WIN )
             this.state.msg = "CONGRATULATIONS";   
     }
 };
@@ -82,30 +82,30 @@ Game.prototype.render = function() {
     ctx.fillStyle = "yellow";
     ctx.font = "38px serif";
     ctx.textAlign="left";
-    ctx.fillText("Score: " + (this.statistics.score).toString(), 10, this.board.box_height+20);
+    ctx.fillText("Score: " + (this.statistics.score).toString(), 10, this.board.BOX_HEIGHT+20);
 
     //Lives legend on screen
     ctx.fillStyle = "red";
     ctx.font = "28px serif";
     ctx.textAlign="left";
-    ctx.drawImage(Resources.get(this.statistics.lives_img), this.board.width-60 , this.board.height-70);  
-    ctx.fillText((this.statistics.lives).toString(), this.board.width-20, this.board.height-30);
+    ctx.drawImage(Resources.get(this.statistics.lives_img), this.board.WIDTH-60 , this.board.HEIGHT-70);  
+    ctx.fillText((this.statistics.lives).toString(), this.board.WIDTH-20, this.board.HEIGHT-30);
 
     //Display gem
     if( this.gem.on_board ) {
-        ctx.drawImage(Resources.get(this.gem.sprite), this.gem.x * this.board.box_width , this.gem.y * this.board.box_height );  
+        ctx.drawImage(Resources.get(this.gem.sprite), this.gem.x * this.board.BOX_WIDTH , this.gem.y * this.board.BOX_HEIGHT );  
     }
 
     //Display heart
     if( this.heart.on_board ) {
-        ctx.drawImage(Resources.get(this.heart.sprite), this.heart.x * this.board.box_width , this.heart.y * this.board.box_height );  
+        ctx.drawImage(Resources.get(this.heart.sprite), this.heart.x * this.board.BOX_WIDTH , this.heart.y * this.board.BOX_HEIGHT );  
     }
 
     //If the game has finished, GAME OVER / CONGRATULATIONS message.
     if( this.state.finished ) {           
 
             ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-            ctx.fillRect(0, this.board.height / 2 - 100, this.board.width, 200);
+            ctx.fillRect(0, this.board.HEIGHT / 2 - 100, this.board.WIDTH, 200);
 
 
             ctx.font = "40px serif";
@@ -113,10 +113,10 @@ Game.prototype.render = function() {
 
             ctx.strokeStyle = "white";
             ctx.lineWidth = 4;
-            ctx.strokeText(this.state.msg, this.board.width / 2, this.board.height / 2);
+            ctx.strokeText(this.state.msg, this.board.WIDTH / 2, this.board.HEIGHT / 2);
             
             ctx.fillStyle = "red"; 
-            ctx.fillText(this.state.msg, this.board.width / 2, this.board.height / 2);
+            ctx.fillText(this.state.msg, this.board.WIDTH / 2, this.board.HEIGHT / 2);
             
             
             this.state.count++;
@@ -132,7 +132,7 @@ Game.prototype.render = function() {
             ctx.textAlign="center";
 
             ctx.fillStyle = this.state.toggle;
-            ctx.fillText("Press enter to restart", this.board.width / 2, this.board.height / 2 + 40);
+            ctx.fillText("Press enter to restart", this.board.WIDTH / 2, this.board.HEIGHT / 2 + 40);
     }
 };
 
@@ -155,20 +155,20 @@ var Enemy = function() {
 Enemy.prototype.initialize = function() {
     this.sprite = 'images/enemy-bug.png';
     this.x = 0;
-    this.y = 1 + Math.floor( game.board.blocks * Math.random() );
-    this.speed = game.board.min_speed + Math.floor( (game.board.max_speed - game.board.min_speed) * Math.random() );
+    this.y = 1 + Math.floor( game.board.BLOCKS * Math.random() );
+    this.speed = game.board.MIN_SPEED + Math.floor( (game.board.MAX_SPEED - game.board.MIN_SPEED) * Math.random() );
 };
 
 Enemy.prototype.update = function(dt) {
     this.x = Math.floor( this.x + dt * this.speed );
 
-    if( this.x >= game.board.width ) {
+    if( this.x >= game.board.WIDTH ) {
         this.initialize();
     }
 };
 
 Enemy.prototype.render = function() {
-    ctx.drawImage( Resources.get( this.sprite), this.x, this.y * game.board.box_height );
+    ctx.drawImage( Resources.get( this.sprite), this.x, this.y * game.board.BOX_HEIGHT );
 };
 
 
@@ -181,23 +181,23 @@ var Player = function() {
 Player.prototype.initialize = function() {
     this.sprite = "images/char-boy.png";
     this.x = 2;
-    this.y = game.board.rows - 1;
+    this.y = game.board.ROWS - 1;
     this.dx = 0;
     this.dy = 0;
 };
 
 Player.prototype.update = function(dt) {
-    this.x = Math.min( Math.max( Math.floor( this.x + this.dx) , 0), game.board.cols - 1 );
-    this.y = Math.min( Math.max( Math.floor( this.y + this.dy) , 0), game.board.rows - 1 );
+    this.x = Math.min( Math.max( Math.floor( this.x + this.dx) , 0), game.board.COLS - 1 );
+    this.y = Math.min( Math.max( Math.floor( this.y + this.dy) , 0), game.board.ROWS - 1 );
 
     this.dx = 0;
     this.dy = 0;
 
     //Collision
     for(var i = 0; i < allEnemies.length ; i++) {
-        if( Math.abs(this.x * game.board.box_width - allEnemies[i].x ) < 50 && (this.y == allEnemies[i].y) ) {
+        if( Math.abs(this.x * game.board.BOX_WIDTH - allEnemies[i].x ) < 50 && (this.y == allEnemies[i].y) ) {
             game.update_lives( -1 );
-            player.initialize();
+            this.initialize();
             break;
         }
     }
@@ -205,7 +205,7 @@ Player.prototype.update = function(dt) {
     //Water
     if( this.y === 0) {
         game.update_score( 1 );
-        player.initialize();
+        this.initialize();
     }
 
     //Gem
@@ -222,7 +222,7 @@ Player.prototype.update = function(dt) {
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x * game.board.box_width , this.y * game.board.box_height );  
+    ctx.drawImage(Resources.get(this.sprite), this.x * game.board.BOX_WIDTH , this.y * game.board.BOX_HEIGHT );  
 };
 
 Player.prototype.handleInput = function( key ) {
@@ -248,7 +248,7 @@ Player.prototype.handleInput = function( key ) {
                 allEnemies.forEach(function(enemy) {
                     enemy.initialize();
                 });
-                player.initialize();
+                this.initialize();
             }
             break;
     }
@@ -263,7 +263,7 @@ Player.prototype.handleInput = function( key ) {
 game = new Game();
 
 allEnemies = [];
-for(var i =0; i < game.board.enemies; i++) {
+for(var i =0; i < game.board.ENEMIES; i++) {
     allEnemies.push( new Enemy() );
 }
 
