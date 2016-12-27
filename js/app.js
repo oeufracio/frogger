2c969f3e2453dc4cac5e9c19659057560339f95d
oeufracio <odin.eufracio@gmail.com>
2016-12-26 23:12:37 -0600

    Bugs and Player movements

--- js/app.js
+++ js/app.js
@@ -1,35 +1,100 @@
-// Enemies our player must avoid
+/*************** Game Object ***************/
+var Game = function() {
+    this.board = {
+        "width": 505,       // Width of Canvas
+        "height": 606,      // Height of Canvas
+        "rows": 6,          // Total number of rows in the game
+        "cols": 5,          // Total number of cols in the game
+        "box_width": 101,   // Width of images
+        "box_height": 83,   // Height of images
+        "blocks": 3,        // Number of available rows for bugs
+        "enemies": 3,       // Number of bugs
+        "min_speed": 200,   // Min speed for bugs
+        "max_speed": 500    // Max speed for bugs
+    };
+};
+
+
+
+/*************** Enemy Object ***************/
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    this.initialize();
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+Enemy.prototype.initialize = function() {
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 1 + Math.floor( game.board.blocks * Math.random() );
+    this.speed = game.board.min_speed + Math.floor( (game.board.max_speed - game.board.min_speed) * Math.random() );
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x = Math.floor( this.x + dt * this.speed );
+
+    if( this.x >= game.board.width ) {
+        this.initialize();
+    }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage( Resources.get( this.sprite), this.x, this.y * game.board.box_height );
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+
+/*************** Player Object ***************/
+var Player = function() {
+    this.initialize();
+};
+
+Player.prototype.initialize = function() {
+    this.sprite = "images/char-boy.png";
+    this.x = 2;
+    this.y = game.board.rows - 1;
+    this.dx = 0;
+    this.dy = 0;
+};
+
+Player.prototype.update = function(dt) {
+    this.x = Math.min( Math.max( Math.floor( this.x + this.dx) , 0), game.board.cols - 1 );
+    this.y = Math.min( Math.max( Math.floor( this.y + this.dy) , 0), game.board.rows - 1 );
+
+    this.dx = 0;
+    this.dy = 0;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x * game.board.box_width , this.y * game.board.box_height );  
+};
+
+Player.prototype.handleInput = function( key ) {
+    
+    switch( key )
+    {
+        case 37:
+            this.dx = -1;
+            break;
+        case 38:
+            this.dy = -1;
+            break;
+        case 39:
+            this.dx = 1;
+            break;
+        case 40:
+            this.dy = 1;
+            break;
+    }
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+game = new Game();
 
+allEnemies = []
+for(var i =0; i < game.board.enemies; i++) {
+    allEnemies.push( new Enemy() );
+}
+
+player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -42,5 +107,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput( e.keyCode );
 });
